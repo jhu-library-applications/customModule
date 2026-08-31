7088923407a6fe07466ee1faa47c5c326c1301dc
Jamie Little <jamie@jamielittle.org>
2026-08-31 12:19:31 -0500

    Angular material button styling

--- src/app/nde-sign-in-bar-custom/nde-sign-in-bar-custom.component.ts
+++ src/app/nde-sign-in-bar-custom/nde-sign-in-bar-custom.component.ts
@@ -4,20 +4,33 @@ import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 type user = { isLoggedIn: boolean };
 export const isLoggedInState = createFeatureSelector<user>('user');
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'custom-nde-sign-in-bar-custom',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIcon],
   templateUrl: './nde-sign-in-bar-custom.component.html',
   styleUrl: './nde-sign-in-bar-custom.component.scss'
 })
 export class NdeSignInBarCustomComponent {
   public store = inject(Store);
 
+
+
   isLoggedIn: boolean = false;
   isLoggedInState$: Observable<user> | undefined;
+  dismissed: boolean = false;
+
+  dismiss() {
+    this.dismissed = true;
+  }
+  
   ngOnInit() {
+
+
+
     this.isLoggedInState$ = this.store.select(isLoggedInState);
     this.isLoggedInState$.subscribe((u) => {
       this.isLoggedIn = u.isLoggedIn;
